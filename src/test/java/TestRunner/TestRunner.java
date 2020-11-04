689d8dd000879af2ea8356ba4f6806ea57a95b58
abhayashokjoshi <abhay.ashokjoshi@gmail.com>
2020-11-04 09:51:46 +0530

    shivani changes

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -17,7 +17,7 @@ import cucumber.api.junit.Cucumber;
 		//,plugin = {"pretty", "html:target/Destination"} 
 		//,plugin = { "pretty", "html:target/cucumber-reports" }
 		 ,plugin = { "pretty", "html:target/cucumber-html-report","json:target/cucumber.json" }
-		 ,tags = {"@smoke"}////or
+		 ,tags = {"@sanity"}////or
 		// tags = {"@Smoke,@sanity"},////or
 		 //tags = {"@Smoke","@sanity"},////and
 		 //tags = {"@Smoke","~@Sanity"},////And
